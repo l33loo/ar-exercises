156b845fc7fc62a908642115fec608c1f9a9b851
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-24 23:35:30 +0000

    Add completed exercise

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,16 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.first
+
+# puts "First store: #{@store1}"
+
+@store2 = Store.find(2)
+
+# puts "Second store: #{@store2}"
+
+@store1.update(name: "Burny")
+
+# puts @store2
+
+# puts "All stores: #{Store.all}"
