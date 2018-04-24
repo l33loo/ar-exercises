fcccba554bd263b91a5d97412ca4b7ded0a674aa
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-24 23:37:49 +0000

    Delete unecessary comments

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -10,7 +10,3 @@ puts "----------"
 @store3.destroy
 
 puts Store.count
-
-# 1. Load the third store (into `@store3`) as you did the other two before.
-# 2. Using Active Record's `destroy` method, delete the store from the database.
-# 3. Verify that the store has been deleted by again outputting (`puts`ing) the `count` (as you did in Exercise 1.)
