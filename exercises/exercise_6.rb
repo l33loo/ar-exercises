c0280168b288e51432cce067912108a44b7cb19e
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-25 01:23:10 +0000

    Change employees' hourly rates to make exercise 7 work

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,14 +8,14 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-@store1.employees.create(first_name: 'Lila', last_name: 'Dude', hourly_rate: 20)
+@store1.employees.create(first_name: 'Lila', last_name: 'Dude', hourly_rate: 40)
 
-@store1.employees.create(first_name: 'Justin', last_name: 'Spinach', hourly_rate: 12)
+@store1.employees.create(first_name: 'Justin', last_name: 'Spinach', hourly_rate: 42)
 
-@store2.employees.create(first_name: 'Joe', last_name: 'Bloke', hourly_rate: 22)
+@store2.employees.create(first_name: 'Joe', last_name: 'Bloke', hourly_rate: 42)
 
-@store2.employees.create(first_name: 'Fanny', last_name: 'Lady', hourly_rate: 32)
+@store2.employees.create(first_name: 'Fanny', last_name: 'Lady', hourly_rate: 52)
 
-@store2.employees.create(first_name: 'Tom', last_name: 'Welling', hourly_rate: 3)
+@store2.employees.create(first_name: 'Tom', last_name: 'Welling', hourly_rate: 53)
 
-@store2.employees.create(first_name: 'Tiffany', last_name: 'Fancy', hourly_rate: 26)
+@store2.employees.create(first_name: 'Tiffany', last_name: 'Fancy', hourly_rate: 66)
